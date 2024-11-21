93ae47b8b12ba288dff3f872338ab99679040a4c
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-11-21 23:58:53 +0900

    remove: Effect Manager의 오브젝트 풀링 삭제

--- Assets/1_Script/Managers/EffectManager.cs
+++ Assets/1_Script/Managers/EffectManager.cs
@@ -41,81 +41,8 @@ namespace Class
         #endregion
 
 
-        // TODO: 오브젝트 풀링과 이펙트의 발동을 다루는 로직을 만들어야 함.
-        // Sound Manager의 예를 참고하여 구현할 것.
+        // TODO: 이펙트의 발동을 다루는 로직을 만들어야 함.
 
-        /*
-
-        #region Object Pooling
-
-        private Transform poolRoot;
-        private Stack<AudioSource> audioSources = new Stack<AudioSource>();
-
-        private void InitPool(int cnt = 10)
-        {
-            poolRoot = new GameObject { name = "_poolRoot" }.transform;
-            poolRoot.parent = this.transform;
-
-            for (int i = 0; i < cnt; i++)
-            {
-                audioSources.Push(Create());
-            }
-        }
-
-        private AudioSource Create()
-        {
-            GameObject go = Instantiate(audioPrefab);
-            go.name = audioPrefab.name;
-            go.transform.parent = poolRoot;
-            go.gameObject.SetActive(false);
-            return go.GetComponent<AudioSource>();
-        }
-        private void Push(AudioSource source)
-        {
-            source.gameObject.SetActive(false);
-            audioSources.Push(source);
-        }
-        private AudioSource Pop()
-        {
-            AudioSource source;
-            if (audioSources.Count() == 0) source = Create();
-            else source = audioSources.Pop();
-
-            source.gameObject.SetActive(true);
-            return source;
-        }
-
-        private IEnumerator PushAfterDelay(AudioSource source, float delay)
-        {
-            yield return new WaitForSeconds(delay);
-            Push(source);
-        }
-
-        #endregion 
-
-        private void Awake()
-        {
-            Init();
-            InitPool();
-        }
-
-        [Header("GameObjects")]
-        [SerializeField] private GameObject audioPrefab;
-
-        [Header("AudioClips")]
-        [SerializeField] public AudioClip[] sfxClips;
-
-        public void CreateAudioSource(Vector3 pos, SfxClipTypes clipIdx)
-        {
-            AudioSource audioSource = Pop();
-            audioSource.transform.position = pos;
-            audioSource.clip = sfxClips[(int)clipIdx];
-            audioSource.volume = 1f;
-            audioSource.Play();
-
-            StartCoroutine(PushAfterDelay(audioSource, audioSource.clip.length));
-        }
-        */
 
 
     }
