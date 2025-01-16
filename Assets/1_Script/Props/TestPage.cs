4b72268ebd011b133b5346d4b85050175d8807fa
Inho <yeoinho75@gmail.com>
2025-01-16 21:53:34 +0900

    feat: add flipping book, omr card

---
+++ Assets/1_Script/Props/TestPage.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Class
+{
+    public class TestPage : MonoBehaviour
+    {
+        [SerializeField] private Transform frontQuad;
+        [SerializeField] private Transform backQuad;
+
+        // TODO - Material이나 스프라이트 받아서 Quad 렌더러에 설정해야됩니다.
+        public void SetPages()
+        {
+
+        }
+    }
+}
